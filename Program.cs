54992d8b3af646cfee17ce6bf8dd2f0a1bdf28dc
Miles <krubbles@gmail.com>
2026-03-01 21:29:55 -0800

    Synced main with solved branch

--- Program.cs
+++ Program.cs
@@ -2,16 +2,23 @@ namespace Game1;
 
 public class Program
 {
-    public const float Threshold = 222;
+    public const float Threshold = 223.5f;
     public const int GameCount = 2000;
     
     public static void Main(string[] args)
     {
-        if (!(args.Length > 0 && args[0] == "evaluate"))
+        bool evaluate = args.Length > 0 && args[0] == "evaluate";
+        if (evaluate)
+        {
+            Console.WriteLine("Staring test");
+            float averageScore = Test.Run(gameCount: GameCount);
+            Console.WriteLine($"Average Score: {averageScore}");
+            Console.WriteLine($"Passed: {averageScore > Threshold}");
+
+        }
+        else
+        {
             Solution.Experiment(); // implement this function to get debug data
-        Console.WriteLine("Staring test");
-        float averageScore = Test.Run(gameCount: GameCount);
-        Console.WriteLine($"Average Score: {averageScore}");
-        Console.WriteLine($"Passed: {averageScore > Threshold}");
+        }
     }
 }
