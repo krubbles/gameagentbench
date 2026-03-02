2adc31a28c6b6914850cc002c5bf0d42b736679f
Miles <krubbles@gmail.com>
2026-03-01 20:07:35 -0800

    moved magic numbers into constants in Program

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@ namespace Game1;
 
 public class Program
 {
+    public const float Threshold = 215;
+    public const int GameCount = 500;
+    
     public static void Main(string[] args)
     {
         if (!(args.Length > 0 && args[0] == "evaluate"))
             Solution.Experiment(); // implement this function to get debug data
         Console.WriteLine("Staring test");
-        float averageScore = Test.Run(500);
+        float averageScore = Test.Run(gameCount: GameCount);
         Console.WriteLine($"Average Score: {averageScore}");
-        Console.WriteLine($"Passed: {averageScore > 215}");
+        Console.WriteLine($"Passed: {averageScore > Threshold}");
     }
 }
